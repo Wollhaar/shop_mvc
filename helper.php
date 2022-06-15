02ae4ec0977ea8f48a0898ef9c9497ac289d49bc
davidgoraj <david.goraj@cec.valantic.com>
2022-06-15 16:16:37 +0200

    0.3.1 (amend) - added smarty to project

--- helper.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-function inputHTML(string $content, string $template, $mark):void
-{
-    $file_content = file_get_contents($template);
-    $file_content = str_replace($mark, $content, $file_content);
-    file_put_contents($template, $file_content);
-}
