1f43529a0e29436d8bf6f10d29dcc5cb38b172ca
davidgoraj <david.goraj@cec.valantic.com>
2022-06-21 14:46:20 +0200

    0.5.2 - modified index.php for open/closed principle

--- index.php
+++ index.php
@@ -1,24 +1,22 @@
 <?php declare(strict_types=1);
 include_once './config.php';
 
-use Shop\Controller\{HomeController, CategoryController, ProductController};
+use Shop\Controller\HomeController;
+use Shop\Service\ControllerProvider;
 
 $request = $_REQUEST;
-$page = $request['page'] ?? '';
 
-switch ($page)
-{
-    case 'category':
-        $controller = new CategoryController();
+$page = $request['page'] ?? 'home';
+$page = requestClass_translation[$page] ?? '';
+
+$controller = new HomeController();
+$provider = new ControllerProvider();
+$controllerList = $provider->getList();
+
+foreach ($controllerList as $controllerName) {
+    if (str_contains($controllerName, $page) && class_exists($controllerName)) {
+        $controller = new $controllerName();
         break;
-
-    case 'detail':
-        $controller = new ProductController();
-        break;
-
-    default:
-        $controller = new HomeController();
+    }
 }
-
-
 $controller->view();
